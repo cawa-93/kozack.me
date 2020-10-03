2717d6924cc71a3820a82347f3df2cf6071366b1
Alex <kozackunisoft@gmail.com>
2020-10-03 18:59:56 +0300

    Уменьшено количество ссылок на профили

--- src/includes/schema.11ty.js
+++ src/includes/schema.11ty.js
@@ -3,16 +3,10 @@ module.exports = ({config}) => {
 	const sideProfiles = [
 		'https://www.facebook.com/alex.kozack',
 		'https://qna.habr.com/user/Kozack',
-		'https://shikimori.org/Alex%20Kozack',
-		'https://shikimori.one/Alex%20Kozack',
-		'https://shikimori.one/Alex+Kozack',
-		'https://shikimori.org/Alex+Kozack',
 		'https://wordpress.org/support/users/cawa-93/',
 		'https://ru.wordpress.org/support/users/cawa-93/',
 		'https://addons.mozilla.org/uk/firefox/user/13317680/',
-		'https://codesandbox.io/u/cawa-93',
 		'https://www.linkedin.com/in/alex-kozack-3a62a665/',
-		'https://www.youtube.com/channel/UCqCVff90SH3jpMDYSP-gDXw'
 	]
 
 	const base = {
