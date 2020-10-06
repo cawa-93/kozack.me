4bece908ce7ad5f68e2f41fae05ccdc05e07524c
Alex <kozackunisoft@gmail.com>
2020-10-06 14:27:09 +0300

    Исправлена ошибка чтения .env когда файла нет

--- src/data/env.js
+++ src/data/env.js
@@ -1,16 +1,7 @@
-const path = require('path');
-const fs = require('fs');
-const dotenv = require('dotenv');
+require('dotenv').config()
 
-module.exports = function() {
-	const envConfig = dotenv.parse(fs.readFileSync(path.resolve(process.cwd(), '.env.local')));
-	for (const k in envConfig) {
-		process.env[k] = envConfig[k];
-	}
-
-	return {
-		URL: process.env.URL || 'http://localhost:8080',
-		FLIP_CARD: JSON.parse(process.env.FLIP_CARD),
-	};
-};
+module.exports = {
+	URL: process.env.URL || 'http://localhost:8080',
+	FLIP_CARD: JSON.parse(process.env.FLIP_CARD),
+}
 
