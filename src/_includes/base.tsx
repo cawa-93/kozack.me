17ccbb41a473bfd43ec143521980e9d986e5a472
Alex Kozack <cawa-93@users.noreply.github.com>
2023-10-20 15:18:14 +0300

    remove components css

--- src/_includes/base.tsx
+++ src/_includes/base.tsx
@@ -2,10 +2,7 @@ export default ({ title, children }) => (
   <html>
     <head>
       <title>{title}</title>
-
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
-        <link rel="stylesheet" href="/components.css" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       <link rel="stylesheet" href="/styles.css" />
     </head>
     <body className={"bg-white dark:bg-zinc-900 scroll-my-20 dark:text-[rgb(226,226,226)]"}>
